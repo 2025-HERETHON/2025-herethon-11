a21a86d4f630295f26474bdcce97068d74022444
Linaeformin <minsa030407@gmail.com>
2025-07-05 19:14:45 +0900

    [FEAT] 더미데이터 파일 생성

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, SubCategory
+from .models import Product, SubCategory, Category
 
 admin.site.register(Product)
 admin.site.register(SubCategory)
+admin.site.register(Category)
 
