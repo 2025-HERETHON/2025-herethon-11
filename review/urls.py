7360377568a835891d1e010a0464b2f3bfbfe2cd
kayubin <kayubin@gmail.com>
2025-07-09 20:30:27 +0900

    [feat]BE_review 앱 생성

---
+++ review/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import create_review, update_review, delete_reveiw, filter_review
+
+urlpatterns = [
+
+]
---
+++ review/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#리뷰 등록
+def create_review(request):
+
+#리뷰 수정
+def update_review(request):
+
+#리뷰 삭제
+def delete_review(request):
+
+#리뷰 필터링
+def filter_review(request):
