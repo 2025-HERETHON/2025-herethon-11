a1a90918719b0d83b41bd1d8f39678e389dcbf01
Linaeformin <minsa030407@gmail.com>
2025-07-12 01:17:00 +0900

    [FEAT] BE_검색 체형 불러오기

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views.wear_views import toggle_wear
 from .views.detail_views import product_detail, detail_toggle_like
 from .views.home_views import product_option_modal, product_search, wear_modal, home
 from .views.products_review_views import product_review, review_product_toggle_like
-from .views.search_views import toggle_like, search_filter
+from .views.search_views import toggle_like, search_filter, get_user_body_info
 urlpatterns = [
     path('', home, name='home'),
     path('like/<int:product_id>/', toggle_like, name='toggle_like'),
@@ -18,6 +18,7 @@ path("products/wear-modal/<int:product_id>/", wear_modal, name="wear_modal"),
     path('products/<int:product_id>/like/', toggle_like, name='toggle_like'),
     path('search/filter/', search_filter, name='search_filter'),
     path('products/review/<int:product_id>/like/', review_product_toggle_like, name='product_like_toggle'),
+path("api/user/body/", get_user_body_info, name="user_body_info"),
 
 ]
 
